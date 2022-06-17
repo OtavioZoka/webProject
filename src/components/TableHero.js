5ebf5430ceec5b94621c952bebaf6f030f55c5c3
Alfafis <ctpmmg2009_101@hotmail.com>
2022-06-17 16:36:52 -0300

    fix: card loading removed

--- src/components/TableHero.js
+++ src/components/TableHero.js
@@ -12,7 +12,6 @@ import {
 } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import CardHero from './CardHero'
-import gifLoading from '../img/marvel-avengers.gif'
 import imgReplace from '../img/hero_default.png'
 
 const BoxStyle = {
@@ -45,7 +44,7 @@ const CardMediaStyle = {
   width: '250px',
 }
 
-const TableHero = ({ items, isLoading }) => {
+const TableHero = ({ items }) => {
   const [openModal, setOpenModal] = useState(false)
   const [informationCard, setInformationCard] = useState([])
   const imgHero = informationCard?.imagemForHero
@@ -56,109 +55,93 @@ const TableHero = ({ items, isLoading }) => {
   }
 
   return (
-    <>
-      {!isLoading ? (
-        <Box style={BoxStyleGif}>
-          <img height="250" src={gifLoading} />
-        </Box>
-      ) : (
-        <section className="contents">
-          {items.map((item) => (
-            <CardHero
-              key={item.id}
-              item={item}
-              openModal={setOpenModal}
-              information={setInformationCard}
-            ></CardHero>
-          ))}
+    <section className="contents">
+      {items.map((item) => (
+        <CardHero
+          key={item.id}
+          item={item}
+          openModal={setOpenModal}
+          information={setInformationCard}
+        ></CardHero>
+      ))}
 
-          <Modal
-            className="Mui-Modal"
-            open={openModal}
-            onClose={handleCloseModal}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={BoxStyle}>
-              <Box
-                style={{ height: '100%', color: 'black', overflowY: 'auto' }}
-              >
-                <ListItem sx={{ pb: 0 }}>
-                  <ListItemText>Name: {infoHero?.name}</ListItemText>
-                </ListItem>
-                <ListItem sx={{ pb: 0, pt: 0 }}>
-                  <ListItemText>Biography:</ListItemText>
-                </ListItem>
-                <ListItem sx={{ pb: 0, pt: 0 }}>
-                  <ListItemText>
-                    {infoHero?.description.length > 0
-                      ? infoHero?.description
-                      : 'Não há Descrição para o herói'}
-                  </ListItemText>
-                </ListItem>
-                <ListItem sx={{ pb: 0, pt: 0 }} style={{ display: 'block' }}>
-                  {infoHero?.comics?.items.length > 0 ? (
-                    <Accordion
-                      sx={{ boxShadow: 'none', padding: 0, margin: 0 }}
+      <Modal
+        className="Mui-Modal"
+        open={openModal}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={BoxStyle}>
+          <Box style={{ height: '100%', color: 'black', overflowY: 'auto' }}>
+            <ListItem sx={{ pb: 0 }}>
+              <ListItemText>Name: {infoHero?.name}</ListItemText>
+            </ListItem>
+            <ListItem sx={{ pb: 0, pt: 0 }}>
+              <ListItemText>Biography:</ListItemText>
+            </ListItem>
+            <ListItem sx={{ pb: 0, pt: 0 }}>
+              <ListItemText>
+                {infoHero?.description.length > 0
+                  ? infoHero?.description
+                  : 'Não há Descrição para o herói'}
+              </ListItemText>
+            </ListItem>
+            <ListItem sx={{ pb: 0, pt: 0 }} style={{ display: 'block' }}>
+              {infoHero?.comics?.items.length > 0 ? (
+                <Accordion sx={{ boxShadow: 'none', padding: 0, margin: 0 }}>
+                  <AccordionSummary
+                    sx={{ padding: 0, margin: 0 }}
+                    id="panel1a-header"
+                    aria-controls="panel1a-content"
+                    expandIcon={<ExpandMoreIcon sx={{ fontSize: '42px' }} />}
+                  >
+                    <Tooltip
+                      title="Clique para ver mais"
+                      placement="bottom-start"
                     >
-                      <AccordionSummary
-                        sx={{ padding: 0, margin: 0 }}
-                        id="panel1a-header"
-                        aria-controls="panel1a-content"
-                        expandIcon={
-                          <ExpandMoreIcon sx={{ fontSize: '42px' }} />
-                        }
+                      <ListItem sx={{ pb: 0, pt: 0, padding: 0, margin: 0 }}>
+                        <ListItemText>Comics:</ListItemText>
+                      </ListItem>
+                    </Tooltip>
+                  </AccordionSummary>
+                  {infoHero?.comics?.items.map((comics) => (
+                    <AccordionDetails>
+                      <ListItemText
+                        sx={{ pb: 0, pt: 0 }}
+                        key={comics?.resourceURI}
                       >
-                        <Tooltip
-                          title="Clique para ver mais"
-                          placement="bottom-start"
-                        >
-                          <ListItem
-                            sx={{ pb: 0, pt: 0, padding: 0, margin: 0 }}
-                          >
-                            <ListItemText>Comics:</ListItemText>
-                          </ListItem>
-                        </Tooltip>
-                      </AccordionSummary>
-                      {infoHero?.comics?.items.map((comics) => (
-                        <AccordionDetails>
-                          <ListItemText
-                            sx={{ pb: 0, pt: 0 }}
-                            key={comics?.resourceURI}
-                          >
-                            {comics?.name}
-                          </ListItemText>
-                        </AccordionDetails>
-                      ))}
-                    </Accordion>
-                  ) : (
-                    <>
-                      <ListItemText>Comics:</ListItemText>
-                      <ListItemText sx={{ pb: 0, pt: 0 }}>
-                        Não há registro de quadrinhos para o herói
+                        {comics?.name}
                       </ListItemText>
-                    </>
-                  )}
-                </ListItem>
-              </Box>
-              <Box>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={imgHero}
-                  sx={CardMediaStyle}
-                  onError={({ currentTarget }) => {
-                    currentTarget.onerror = null
-                    currentTarget.src = imgReplace
-                  }}
-                  alt={`imagem do heroi ${infoHero?.name}`}
-                />
-              </Box>
-            </Box>
-          </Modal>
-        </section>
-      )}
-    </>
+                    </AccordionDetails>
+                  ))}
+                </Accordion>
+              ) : (
+                <>
+                  <ListItemText>Comics:</ListItemText>
+                  <ListItemText sx={{ pb: 0, pt: 0 }}>
+                    Não há registro de quadrinhos para o herói
+                  </ListItemText>
+                </>
+              )}
+            </ListItem>
+          </Box>
+          <Box>
+            <CardMedia
+              component="img"
+              height="140"
+              image={imgHero}
+              sx={CardMediaStyle}
+              onError={({ currentTarget }) => {
+                currentTarget.onerror = null
+                currentTarget.src = imgReplace
+              }}
+              alt={`imagem do heroi ${infoHero?.name}`}
+            />
+          </Box>
+        </Box>
+      </Modal>
+    </section>
   )
 }
 
